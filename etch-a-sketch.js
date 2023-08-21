1e04af6e9c3545b49cb41e6782988a4cc2bc3193
BDS <113209967+BDS-001@users.noreply.github.com>
2023-08-21 15:17:23 -0400

    added mouse over eventlistenenrs to the block divs to allow drawing

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -7,7 +7,11 @@ for (i=0;i<16;i++) {
     for (j=0;j<16;j++) {
         const block = document.createElement('div')
         block.className = 'block'
+        block.addEventListener('mouseover', function() {
+            block.style.backgroundColor = 'black'
+        })
         gridRow.appendChild(block)
     }
     grid.appendChild(gridRow)
 }
+
