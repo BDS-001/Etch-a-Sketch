9d26488aa4bf469767e5e901eb7d6ed5b2b7e41a
BDS <113209967+BDS-001@users.noreply.github.com>
2023-08-21 16:02:07 -0400

    added two buttons at the top. one is to change the number of rows/columns in the grid, the other is to clear the grid

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,17 +1,40 @@
 const grid = document.querySelector('#grid')
+const gridSelect = document.querySelector('.selectSize')
+const gridClear = document.querySelector('.gridClear')
+let gridSize = 16;
 
-for (i=0;i<16;i++) {
-    const gridRow = document.createElement('div')
-    gridRow.className = 'row'
-
-    for (j=0;j<16;j++) {
-        const block = document.createElement('div')
-        block.className = 'block'
-        block.addEventListener('mouseover', function() {
-            block.style.backgroundColor = 'black'
-        })
-        gridRow.appendChild(block)
+gridSelect.addEventListener('click', function() {
+    let size = parseInt(prompt("how many squars should the grid be?"))
+    if (size === NaN || size > 100) {
+        gridSize = 16
+    } else {
+        gridSize = size;
     }
-    grid.appendChild(gridRow)
+    grid.innerHTML = '';
+    createGrid(gridSize);
+})
+
+
+gridClear.addEventListener('click', function() {
+    grid.innerHTML = '';
+    createGrid(gridSize);
+})
+
+function createGrid(gridSize) {
+    for (i=0;i<gridSize;i++) {
+        const gridRow = document.createElement('div')
+        gridRow.className = 'row'
+    
+        for (j=0;j<gridSize;j++) {
+            const block = document.createElement('div')
+            block.className = 'block'
+            block.addEventListener('mouseover', function() {
+                block.style.backgroundColor = 'black'
+            })
+            gridRow.appendChild(block)
+        }
+        grid.appendChild(gridRow)
+    }
 }
 
+createGrid(gridSize);
